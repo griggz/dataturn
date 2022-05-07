8998f53b21648595d7fdc45da2437f7b4db13123
waynegrigsby <wgrigsby@unfoundation.org>
2022-05-07 19:49:40 -0400

    added env vars to next config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,13 @@ module.exports = {
   reactStrictMode: false,
   env: {
     ENV: process.env.ENV,
+    DATABASE_URL: process.env.DATABASE_URL,
+    GITHUB_ID: process.env.GITHUB_ID,
+    GITHUB_SECRET: process.env.GITHUB_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    AUTH_SECRET: process.env.AUTH_SECRET,
+    JWT_SECRET: process.env.JWT_SECRET,
   },
   images: {
     domains: ["images.unsplash.com", "source.unsplash.com"],
